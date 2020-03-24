f260b6519d2bd44005118e8d49ee0bdcaf08cbfd
Luisgui <luisguirohi@gmail.com>
2020-03-24 18:10:38 -0500

    Pages e initialprops

---
+++ pages/index.js
@@ -0,0 +1,83 @@
+import 'isomorphic-fetch';
+
+export default class extends React.Component{
+
+    static async getInitialProps(){
+        let req = await fetch('https://api.audioboom.com/channels/recommended')
+        let { body: channels } = await req.json();
+        return { channels };
+    }
+
+    render(){
+        const { channels } = this.props;
+
+        return(
+            <div>
+                <header>Podcasts</header>
+
+                <div className="channels">
+                    {channels.map((channel) => (
+                        <div className="channel">
+                            <img src={channel.urls.logo_image.original} alt={channel.title} />
+                            <h2>{channel.title}</h2>
+                        </div>
+                    ))}
+                </div>
+
+                <style jsx>{`
+                    header {
+                        color: #fff;
+                        background: #8756ca;
+                        padding: 15px;
+                        text-align: center;
+                    }
+                    .channels {
+                        display: grid;
+                        grid-gap: 15px;
+                        padding: 15px;
+                        grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                    }
+                    a.channel {
+                        display: block;
+                        margin-bottom: 0.5em;
+                        color: #333;
+                        text-decoration: none;
+                    }
+                    .channel img {
+                        border-radius: 3px;
+                        box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                        width: 100%;
+                    }
+                    h2 {
+                        padding: 5px;
+                        font-size: 0.9em;
+                        font-weight: 600;
+                        margin: 0;
+                        text-align: center;
+                    }
+                `}</style>
+
+                <h1>Helloooo</h1>
+                <p>Next js</p>
+
+                <img src="/static/platzi-logo.png" alt="Platzi" />
+
+                <style jsx>{`
+                    h1{
+                        color: red;
+                    }
+                    :global(p){
+                        color: green;
+                    }
+                `}</style>
+
+                <style jsx global>{`
+                    body{
+                        background: white;
+                        font-family: system-ui;
+                    }
+                `}</style>
+            </div>
+        )
+    }
+}
